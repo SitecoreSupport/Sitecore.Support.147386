7140fa262f562015996d1162992e9212a63c17f0
DK\obr <obr@sitecore.net>
2018-01-05 13:24:59 +0200

    Init

---
+++ src/Sitecore.Support.147386/ContentSearch/Azure/Query/CloudQueryMapper.cs
@@ -0,0 +1,88 @@
+﻿using Sitecore.ContentSearch.Azure.Http;
+using Sitecore.ContentSearch.Azure.Query;
+using Sitecore.ContentSearch.Azure.Schema;
+using Sitecore.ContentSearch.Linq.Common;
+using Sitecore.ContentSearch.Linq.Helpers;
+using Sitecore.ContentSearch.Linq.Nodes;
+
+namespace Sitecore.Support.ContentSearch.Azure.Query
+{
+    public class CloudQueryMapper : Sitecore.ContentSearch.Azure.Query.CloudQueryMapper
+    {
+        public CloudQueryMapper(CloudIndexParameters parameters) : base(parameters)
+        {
+        }
+        private ICloudSearchIndexSchema Schema
+        {
+            get
+            {
+                return this.Parameters.Schema as ICloudSearchIndexSchema;
+            }
+        }
+
+        protected override string HandleEqual(EqualNode node, CloudQueryMapperState state)
+        {
+            var onlyConstants = node.LeftNode is ConstantNode && node.RightNode is ConstantNode;
+
+            if (onlyConstants)
+            {
+                var comparison = ((ConstantNode)node.LeftNode).Value.Equals(((ConstantNode)node.RightNode).Value);
+
+                var expression = comparison
+                                     ? Sitecore.ContentSearch.Azure.Query.CloudQueryBuilder.Search.SearchForEverything
+                                     : Sitecore.ContentSearch.Azure.Query.CloudQueryBuilder.Search.SearchForNothing;
+
+                return Sitecore.Support.ContentSearch.Azure.Query.CloudQueryBuilder.Search.Operations.Equal(null, expression, node.Boost);
+            }
+
+            var fieldNode = QueryHelper.GetFieldNode(node);
+            var valueNode = QueryHelper.GetValueNode(node, fieldNode.FieldType);
+
+            string query = null;
+            if (this.ProcessAsVirtualField(fieldNode.FieldKey, valueNode.Value, node.Boost, ComparisonType.Equal, state, out query))
+            {
+                return query;
+            }
+
+            return this.HandleEqual(fieldNode.FieldKey, valueNode.Value, node.Boost);
+        }
+
+        private string HandleEqual(string initFieldName, object fieldValue, float boost)
+        {
+            var fieldName = this.Parameters.FieldNameTranslator.GetIndexFieldName(initFieldName, this.Parameters.IndexedFieldType);
+            var fieldSchema = this.Schema.GetFieldByCloudName(fieldName);
+            if (fieldSchema == null)
+            {
+                var expression = fieldValue == null ?
+                    Sitecore.ContentSearch.Azure.Query.CloudQueryBuilder.Search.SearchForEverything :
+                    Sitecore.ContentSearch.Azure.Query.CloudQueryBuilder.Search.SearchForNothing;
+
+                return Sitecore.Support.ContentSearch.Azure.Query.CloudQueryBuilder.Search.Operations.Equal(null, expression, boost);
+            }
+
+            var formattedValue = this.ValueFormatter.FormatValueForIndexStorage(fieldValue, fieldName);
+
+            if (fieldSchema.Type == EdmTypes.StringCollection)
+            {
+                return Sitecore.ContentSearch.Azure.Query.CloudQueryBuilder.Filter.Operations.Collections.Any(fieldName, formattedValue, fieldSchema.Type);
+            }
+
+            if (formattedValue == null)
+            {
+                return $"&$filter={fieldName} eq null";
+            }
+
+            if (formattedValue is string)
+            {
+                if (formattedValue.ToString().Trim() == string.Empty)
+                {
+                    return Sitecore.ContentSearch.Azure.Query.CloudQueryBuilder.Filter.Operations.Equal(fieldName, formattedValue, fieldSchema.Type, boost);
+                }
+
+                return Sitecore.Support.ContentSearch.Azure.Query.CloudQueryBuilder.Search.Operations.Equal(fieldName, formattedValue, boost);
+            }
+
+            return Sitecore.ContentSearch.Azure.Query.CloudQueryBuilder.Filter.Operations.Equal(fieldName, formattedValue, fieldSchema.Type, boost);
+        }
+    }
+}
---
+++ src/Sitecore.Support.147386/ContentSearch/Azure/Query/LinqToCloudIndex.cs
@@ -0,0 +1,42 @@
+﻿namespace Sitecore.Support.ContentSearch.Azure.Query
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Reflection;
+    using Sitecore.ContentSearch.Linq.Common;
+    using Sitecore.ContentSearch.Azure.Query;
+
+    public class LinqToCloudIndex<TItem> : Sitecore.ContentSearch.Azure.Query.LinqToCloudIndex<TItem>
+    {
+        private static ConcurrentDictionary<Type, FieldInfo> queryMapperFieldInfos = new ConcurrentDictionary<Type, FieldInfo>();
+        public LinqToCloudIndex(Sitecore.ContentSearch.Azure.CloudSearchSearchContext context, IExecutionContext executionContext) : base(context, executionContext)
+        {
+        }
+
+        public LinqToCloudIndex(Sitecore.ContentSearch.Azure.CloudSearchSearchContext context, IExecutionContext[] executionContexts) : base(context, executionContexts)
+        {
+            FieldInfo queryMapperFieldInfo;
+            var type = this.GetType().BaseType.BaseType;
+            if (queryMapperFieldInfos.TryGetValue(type, out queryMapperFieldInfo))
+            {
+                queryMapperFieldInfo.SetValue(this, new Sitecore.Support.ContentSearch.Azure.Query.CloudQueryMapper(this.Parameters));
+            }
+            else
+            {
+                queryMapperFieldInfo = type.GetField("queryMapper", BindingFlags.Instance | BindingFlags.NonPublic);
+                queryMapperFieldInfos.TryAdd(type, queryMapperFieldInfo);
+                queryMapperFieldInfo.SetValue(this, new Sitecore.Support.ContentSearch.Azure.Query.CloudQueryMapper(this.Parameters));
+            }
+        }
+
+        private TResult ApplyScalarMethods<TResult, TDocument>(CloudQuery query,
+          CloudSearchResults<TDocument> processedResults, int? totalCount)
+        {
+            Type documentType = typeof(TResult).GetGenericArguments()[0];
+            var applyScalarMethodsMethod = this.GetType().BaseType.GetMethod("ApplyScalarMethods", BindingFlags.Instance | BindingFlags.NonPublic);
+            var applyScalarMethodsGenericMethod =
+              applyScalarMethodsMethod.MakeGenericMethod(new Type[] { typeof(TResult), documentType });
+            return (TResult)applyScalarMethodsGenericMethod.Invoke(this, new object[] { query, processedResults, totalCount });
+        }
+    }
+}
