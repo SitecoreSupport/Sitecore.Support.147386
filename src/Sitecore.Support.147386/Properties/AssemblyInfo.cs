2f8cebdaaef6f8fcbf6c766175ea205bbff3d52e
Oleg Braginskyy <oleg.braginskyy@sitecore.net>
2019-07-02 16:43:25 +0300

    Init

---
+++ src/Sitecore.Support.147386/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.147386")]
+[assembly: AssemblyProduct("Sitecore.Support.147386")]
+[assembly: ComVisible(false)]
